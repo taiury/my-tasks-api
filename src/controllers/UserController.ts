abb5970d36496126ca9243af684362b5df5940fb
taiury <alexandretaiury@gmail.com>
2022-12-20 18:06:24 -0300

    add methods for user

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -6,20 +6,32 @@ const prisma = new PrismaClient();
 
 export interface UserControllerProtocol {
   store(req: Request, res: Response): Promise<Response>;
+  update(req: Request, res: Response): Promise<Response>;
+  index(req: Request, res: Response): Promise<Response>;
 }
 
 export class UserController implements UserControllerProtocol {
   public async store(req: Request, res: Response): Promise<Response> {
     try {
-      const { email, password, name }: User = req.body;
+      const { email, password, name, age }: User = req.body;
+
+      const userAlreadyExists = await prisma.user.findUnique({
+        where: { email },
+      });
+
+      if (userAlreadyExists) {
+        return res.status(401).json({ error: 'email already exists' });
+      }
 
       const user = await prisma.user.create({
         data: {
           email,
           password,
           name,
+          age,
         },
       });
+
       return res.json({
         id: user.id,
         name: user.name,
@@ -27,7 +39,50 @@ export class UserController implements UserControllerProtocol {
         token: generateToken(user.id),
       });
     } catch (error) {
-      return res.status(400).json({ error: 'Email already existe' });
+      return res.status(400).json({ error: 'Bad request' });
+    }
+  }
+
+  public async update(req: Request, res: Response): Promise<Response> {
+    try {
+      const { name, password, age }: User = req.body;
+      const userId = req.userId;
+
+      if (typeof userId !== 'number') {
+        return res.status(400).json({ error: 'Bad request' });
+      }
+
+      const user = await prisma.user.update({
+        where: { id: userId },
+        data: {
+          name,
+          password,
+          age,
+        },
+      });
+
+      return res.json({ user });
+    } catch (error) {
+      return res.status(400).json({ error: 'Bad request' });
+    }
+  }
+
+  public async index(req: Request, res: Response): Promise<Response> {
+    try {
+      const userId = req.userId;
+
+      if (typeof userId !== 'number') {
+        return res.status(400).json({ error: 'Bad request' });
+      }
+
+      const user = await prisma.user.findUnique({
+        where: { id: userId },
+        include: { tasks: { where: { authorId: userId } } },
+      });
+
+      return res.json({ ...user });
+    } catch (error) {
+      return res.status(400).json({ error: 'Bad request' });
     }
   }
 }
