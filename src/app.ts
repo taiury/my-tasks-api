a8581c5725295eddef1b5d4822723f6c8dcf3aaf
taiury <alexandretaiury@gmail.com>
2022-12-23 17:21:31 -0300

    add helmet middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import { routes } from './routes';
+import helmet from 'helmet';
 
 const app = express();
 app.use(cors());
+app.use(helmet());
 app.use(express.json());
 
 app.use(routes);
