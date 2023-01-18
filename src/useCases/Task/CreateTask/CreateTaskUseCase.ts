249ec6205526d3aafc5784b775b3ee7faca825c5
taiury <alexandretaiury@gmail.com>
2023-01-18 17:28:07 -0300

    fix: verify title exists

--- src/useCases/Task/CreateTask/CreateTaskUseCase.ts
+++ src/useCases/Task/CreateTask/CreateTaskUseCase.ts
@@ -5,6 +5,8 @@ import { CreateTaskDTO } from './CreateTaskDTO';
 class CreateTaskUseCase implements UseCaseProtocol<CreateTaskDTO, void> {
   constructor(private readonly taskRepository: TaskRepositoryProtocol) {}
   async execute({ userId, title, description }: CreateTaskDTO): Promise<void> {
+    if (!title) throw new Error('Title is required');
+
     this.taskRepository.add({
       title,
       description,
