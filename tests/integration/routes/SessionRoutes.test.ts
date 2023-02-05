4fa617c2dda18d6b986d6ef50f00c0874178fdde
taiury <alexandretaiury@gmail.com>
2023-02-05 16:00:29 -0300

    test: adding integration tests to feature enable account

--- tests/integration/routes/SessionRoutes.test.ts
+++ tests/integration/routes/SessionRoutes.test.ts
@@ -13,15 +13,32 @@ const newUser = {
 
 describe('SessionRoutes', () => {
   beforeAll(async () => {
-    const userAlreadyExists = await dbClient.user.findUnique({
+    const LoginUserAlreadyExists = await dbClient.user.findUnique({
       where: { email: 'TEST@gmail.com' },
     });
 
-    if (userAlreadyExists) {
+    const EnableAccountUserAlreadyExists = await dbClient.user.findUnique({
+      where: { email: 'Enable_Account@gmail.com' },
+    });
+
+    if (LoginUserAlreadyExists) {
       await dbClient.user.delete({ where: { email: 'TEST@gmail.com' } });
+    }
+    if (EnableAccountUserAlreadyExists) {
+      await dbClient.user.delete({
+        where: { email: 'Enable_Account@gmail.com' },
+      });
     }
 
     await dbClient.user.create({ data: { ...newUser } });
+    await dbClient.user.create({
+      data: {
+        ...newUser,
+        email: 'Enable_Account@gmail.com',
+        isEnabled: false,
+        email_code: 9,
+      },
+    });
   });
 
   // POST /login
@@ -61,6 +78,57 @@ describe('SessionRoutes', () => {
       .expect(400)
       .send({ email: 'USER_INVALID@gmail.com' })
       .send({ password: 'INVALID_PASSWORD' });
+
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        error: 'Bad Request',
+      }),
+    );
+  });
+
+  // POST /enable
+  it('should enable account and return 200', async () => {
+    await request(app)
+      .post('/enable')
+      .expect(200)
+      .send({ email: 'Enable_Account@gmail.com' })
+      .send({ code: 9 });
+  });
+
+  it('should not enable account because email code is invalid', async () => {
+    const response = await request(app)
+      .post('/enable')
+      .expect(401)
+      .send({ email: 'Enable_Account@gmail.com' })
+      .send({ code: 0 });
+
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        error: 'Email code is invalid.',
+      }),
+    );
+  });
+
+  it('should not enable account because email not exists', async () => {
+    const response = await request(app)
+      .post('/enable')
+      .expect(404)
+      .send({ email: 'USER_INVALID@gmail.com' })
+      .send({ code: 9 });
+
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        error: 'User not found.',
+      }),
+    );
+  });
+
+  it('should not enable account because parameter code is undefined', async () => {
+    const response = await request(app)
+      .post('/enable')
+      .expect(400)
+      .send({ email: 'USER_INVALID@gmail.com' })
+      .send({ code: undefined });
 
     expect(response.body).toEqual(
       expect.objectContaining({
