aa5ba1ef23a6081234e992ca7eb32b2ad1a4971d
taiury <alexandretaiury@gmail.com>
2023-02-05 08:52:14 -0300

    feat: added method for request user by Email code

--- src/repositories/UserRepositoryProtocol.ts
+++ src/repositories/UserRepositoryProtocol.ts
@@ -3,6 +3,7 @@ import { User } from '@/entities';
 interface UserRepositoryProtocol {
   findById(userId: number): Promise<Omit<User, 'password'> | null>;
   findByEmail(email: string): Promise<User | null>;
+  findByEmailCode(code: number): Promise<User | null>;
   add(props: User): Promise<void>;
   modify(userId: number, user: User): Promise<void>;
 }
--- src/repositories/implementations/UserRepository.ts
+++ src/repositories/implementations/UserRepository.ts
@@ -4,6 +4,12 @@ import { UserRepositoryProtocol } from '../UserRepositoryProtocol';
 
 class UserRepository implements UserRepositoryProtocol {
   constructor(private readonly dbClient: PrismaClient) {}
+  async findByEmailCode(code: number): Promise<User | null> {
+    const user = await this.dbClient.user.findUnique({
+      where: { email_code: code },
+    });
+    return user as User | null;
+  }
   async findById(userId: number): Promise<Omit<User, 'password'> | null> {
     const user = await this.dbClient.user.findUnique({
       where: { id: userId },
@@ -20,7 +26,7 @@ class UserRepository implements UserRepositoryProtocol {
 
   async findByEmail(email: string): Promise<User | null> {
     const user = await this.dbClient.user.findUnique({ where: { email } });
-    return user;
+    return user as User | null;
   }
 
   async add({ email, password, name, age }: User): Promise<void> {
