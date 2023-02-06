13f0bef8b0089dab32861ae618bd4a631bc09cf6
taiury <alexandretaiury@gmail.com>
2023-02-06 11:38:51 -0300

    refactor: remove parameter check from request body

--- src/controllers/Session/EnableAccountController.ts
+++ src/controllers/Session/EnableAccountController.ts
@@ -1,6 +1,6 @@
 import { ControllerProtocol, UseCaseProtocol } from '@/types';
-import { EnableAccountDTO, enableAccountSchema } from '@/useCases';
-import { Api401Error, Api404Error } from '@/utils';
+import { EnableAccountDTO } from '@/useCases';
+import { Api400Error, Api401Error, Api404Error } from '@/utils';
 import { Request, Response } from 'express';
 
 class EnableAccountController implements ControllerProtocol {
@@ -10,16 +10,16 @@ class EnableAccountController implements ControllerProtocol {
 
   async perform(request: Request, response: Response): Promise<Response> {
     try {
-      const { email, code } = enableAccountSchema.parse(request.body);
+      const { email, code } = request.body;
 
       await this.enableAccount.execute({ email, code });
 
       return response.status(200).json();
     } catch (err) {
+      const isApi400Error = err instanceof Api400Error;
       const isApi401Error = err instanceof Api401Error;
       const isApi404Error = err instanceof Api404Error;
-
-      if (isApi401Error || isApi404Error) {
+      if (isApi400Error || isApi401Error || isApi404Error) {
         return response.status(err.statusCode).json({ error: err.name });
       }
 
